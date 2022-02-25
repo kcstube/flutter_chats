8bc2ed21607b672162dfc0b461f4be60ced268b0
fummicc1 <fumiya.tennis1@gmail.com>
2022-02-25 10:26:40 +0900

    開始地点: 01

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chats/chat_list/page.dart';
-import 'package:flutter_chats/user_settings/page.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,42 +21,24 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.latoTextTheme(),
         bottomSheetTheme: const BottomSheetThemeData(
           backgroundColor: Colors.transparent,
-        )
+        ),
       ),
-      home: FutureBuilder<Widget>(
-        future: selectRootPage(context),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return snapshot.requireData;
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+      home: const Center(
+        child: CircularProgressIndicator(),
       ),
     );
   }
-
-  Future<Widget> selectRootPage(BuildContext context) async {
-    final SharedPreferences sharedPreferences =
-        await SharedPreferences.getInstance();
-    final String? userId = sharedPreferences.getString("user_id");
-    if (userId == null) {
-      return const UserSettingsPage();
-    } else {
-      return ChatListPage(myUserId: userId,);
-    }
-  }
 }
 
-// FIXME: Util関数群
+// 日付をフォーマットして文字列を返す関数
 String formatDateTime(DateTime dateTime) {
   final now = DateTime.now();
-   final bool isSameDay = now.difference(dateTime) < const Duration(days: 1);
+  final bool isSameDay = now.difference(dateTime) < const Duration(days: 1);
   final DateFormat dateFormatter;
-   if (isSameDay) {
-     dateFormatter = DateFormat.Hm();
-   } else {
-     dateFormatter = DateFormat('yyyy/MM/dd HH:mm');
-   }
-   return dateFormatter.format(dateTime);
+  if (isSameDay) {
+    dateFormatter = DateFormat.Hm();
+  } else {
+    dateFormatter = DateFormat('yyyy/MM/dd HH:mm');
+  }
+  return dateFormatter.format(dateTime);
 }
